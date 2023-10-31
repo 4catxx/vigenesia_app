07f7750614c6b027237d9b7e5419a6108a4b7be7
Muhammad Hardithya Aenuddin <muhammadadhi2803@gmail.com>
2023-10-31 10:12:54 +0700

    Penambahan auth API pada register dan Login

--- lib/screen/login_page.dart
+++ lib/screen/login_page.dart
@@ -4,7 +4,8 @@ import 'register_page.dart';
 import 'package:flutter_form_builder/flutter_form_builder.dart';
 import 'package:form_builder_validators/form_builder_validators.dart';
 import 'dashboard_page.dart';
-import 'home_page.dart';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 
 // Inisialisasi GlobalKey untuk FormBuilder
 final _formKey = GlobalKey<FormBuilderState>();
@@ -21,126 +22,168 @@ class _LoginState extends State<Login> {
   TextEditingController emailController = TextEditingController();
   TextEditingController passwordController = TextEditingController();
 
+  // Fungsi untuk mengirim data login ke API
+  Future<String> loginUser(String email, String password) async {
+    final apiUrl = Uri.parse('http://127.0.0.1:80/vigenesia/api/login');
+    final response = await http.post(
+      apiUrl,
+      body: {'email': email, 'password': password},
+    );
+
+    final responseData = json.decode(response.body);
+
+    if (response.statusCode == 200) {
+      bool isActive = responseData['is_active'];
+
+      if (isActive) {
+        return "success";
+      } else {
+        return 'Gagal.';
+      }
+    } else if (response.statusCode == 400) {
+      // Jika status kode HTTP adalah 400, kembalikan pesan yang sesuai
+      if (responseData == "Ada kesalahan di email / password.") {
+        return 'Login gagal. Email atau password salah.';
+      } else if (responseData == "Belum mengisi email dan password.") {
+        return 'Email dan password harus diisi.';
+      } else {
+        return 'Terjadi kesalahan.';
+      }
+    } else {
+      return 'Terjadi kesalahan saat menghubungi server.';
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    title:
-    'First App';
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.brown,
         title: Text('Login'),
       ),
       body: SingleChildScrollView(
-          child: Container(
-              color: Color.fromARGB(255, 255, 255, 255),
-              height: MediaQuery.of(context).size.height,
-              child: Center(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Image.asset('images/4207.jpg',
-                        height: 300.0), // Gambar di atas tulisan "Login"
-                    Text(
-                      'Login',
-                      style:
-                          TextStyle(fontSize: 22, fontWeight: FontWeight.w500),
-                    ),
-
-                    SizedBox(height: 20), //awal script textfield email
-                    Center(
-                      child: FormBuilder(
-                        key: _formKey,
-                        child: Container(
-                          width: MediaQuery.of(context).size.width / 1.3,
-                          child: Column(
-                            children: [
-                              FormBuilderTextField(
-                                name: "email",
-                                controller: emailController,
-                                decoration: InputDecoration(
-                                    contentPadding: EdgeInsets.only(left: 10),
-                                    border: OutlineInputBorder(),
-                                    labelText: "Email"),
-                                validator: FormBuilderValidators.compose([
-                                  FormBuilderValidators
-                                      .required(), // Bidang harus diisi
-                                  FormBuilderValidators
-                                      .email(), // Memastikan bahwa email dalam format yang benar
-                                ]),
+        child: Container(
+          color: Color.fromARGB(255, 255, 255, 255),
+          height: MediaQuery.of(context).size.height,
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Image.asset('images/4207.jpg', height: 300.0),
+                Text(
+                  'Login',
+                  style: TextStyle(fontSize: 22, fontWeight: FontWeight.w500),
+                ),
+                SizedBox(height: 20),
+                Center(
+                  child: FormBuilder(
+                    key: _formKey,
+                    child: Container(
+                      width: MediaQuery.of(context).size.width / 1.3,
+                      child: Column(
+                        children: [
+                          FormBuilderTextField(
+                            name: "email",
+                            controller: emailController,
+                            decoration: InputDecoration(
+                              contentPadding: EdgeInsets.only(left: 10),
+                              border: OutlineInputBorder(),
+                              labelText: "Email",
+                            ),
+                            validator: FormBuilderValidators.compose([
+                              FormBuilderValidators.required(),
+                              FormBuilderValidators.email(),
+                            ]),
+                          ),
+                          SizedBox(height: 20),
+                          FormBuilderTextField(
+                            obscureText: _obscureText,
+                            name: "password",
+                            controller: passwordController,
+                            decoration: InputDecoration(
+                              contentPadding: EdgeInsets.only(left: 10),
+                              border: OutlineInputBorder(),
+                              labelText: "Password",
+                              suffixIcon: IconButton(
+                                icon: Icon(
+                                  _obscureText
+                                      ? Icons.visibility
+                                      : Icons.visibility_off,
+                                ),
+                                onPressed: () {
+                                  setState(() {
+                                    _obscureText = !_obscureText;
+                                  });
+                                },
                               ),
-                              SizedBox(height: 20),
-                              FormBuilderTextField(
-                                obscureText: _obscureText,
-                                name: "password",
-                                decoration: InputDecoration(
-                                  contentPadding: EdgeInsets.only(left: 10),
-                                  border: OutlineInputBorder(),
-                                  labelText: "Password",
-                                  suffixIcon: IconButton(
-                                    icon: Icon(
-                                      // Berdasarkan kondisi, tampilkan ikon yang sesuai
-                                      _obscureText
-                                          ? Icons.visibility
-                                          : Icons.visibility_off,
-                                    ),
-                                    onPressed: () {
-                                      // Ubah nilai _obscureText saat tombol ditekan
-                                      setState(() {
-                                        _obscureText = !_obscureText;
-                                      });
+                            ),
+                            validator: FormBuilderValidators.required(),
+                          ),
+                          SizedBox(height: 30),
+                          Text.rich(
+                            TextSpan(
+                              children: [
+                                TextSpan(
+                                  text: 'Tidak ada akun ? ',
+                                  style: TextStyle(color: Colors.black54),
+                                ),
+                                TextSpan(
+                                  text: 'Sign Up',
+                                  recognizer: TapGestureRecognizer()
+                                    ..onTap = () {
+                                      Navigator.push(
+                                        context,
+                                        MaterialPageRoute(
+                                          builder: (BuildContext context) =>
+                                              Register(),
+                                        ),
+                                      );
                                     },
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.w600,
+                                    color: Colors.blueAccent,
                                   ),
                                 ),
-                                validator: FormBuilderValidators.required(),
-                              ), //akhir tahap 3
-                              SizedBox(height: 30),
-                              Text.rich(
-                                TextSpan(children: [
-                                  TextSpan(
-                                    text: 'Tidak ada akun ? ',
-                                    style: TextStyle(color: Colors.black54),
-                                  ),
-                                  TextSpan(
-                                    text: 'Sign Up',
-                                    recognizer: TapGestureRecognizer()
-                                      ..onTap = () {
-                                        Navigator.push(
-                                            context,
-                                            new MaterialPageRoute(
-                                                builder:
-                                                    (BuildContext context) =>
-                                                        new Register()));
-                                      },
-                                    style: TextStyle(
-                                        fontWeight: FontWeight.w600,
-                                        color: Colors.blueAccent),
-                                  ),
-                                ]),
-                              ), //akhir tahap 4
-                              ElevatedButton(
-                                onPressed: () {
-                                  if (_formKey.currentState
-                                          ?.saveAndValidate() ??
-                                      false) {
-                                    // Validasi berhasil, lakukan tindakan login di sini
-                                    Navigator.push(
-                                      context,
-                                      MaterialPageRoute(
-                                          builder: (BuildContext context) =>
-                                              Dashboard()),
-                                    );
-                                  }
-                                },
-                                child: Text('Login'),
-                              )
-                            ],
+                              ],
+                            ),
                           ),
-                        ),
+                          ElevatedButton(
+                            onPressed: () async {
+                              if (_formKey.currentState?.saveAndValidate() ??
+                                  false) {
+                                String response = await loginUser(
+                                    emailController.text,
+                                    passwordController.text);
+
+                                if (response == "success") {
+                                  Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                      builder: (BuildContext context) =>
+                                          Dashboard(),
+                                    ),
+                                  );
+                                } else {
+                                  ScaffoldMessenger.of(context).showSnackBar(
+                                    SnackBar(
+                                      content: Text(response),
+                                    ),
+                                  );
+                                }
+                              }
+                            },
+                            child: Text('Login'),
+                          )
+                        ],
                       ),
                     ),
-                  ],
+                  ),
                 ),
-              ))),
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
--- lib/screen/register_page.dart
+++ lib/screen/register_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_form_builder/flutter_form_builder.dart';
 import 'package:vigenesia_app/screen/login_page.dart';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 
 class Register extends StatefulWidget {
   const Register({Key? key}) : super(key: key);
@@ -10,10 +12,47 @@ class Register extends StatefulWidget {
 }
 
 class _RegisterState extends State<Register> {
-  TextEditingController nameController = TextEditingController();
+  TextEditingController namaController = TextEditingController();
   TextEditingController profesiController = TextEditingController();
   TextEditingController emailController = TextEditingController();
   TextEditingController passwordController = TextEditingController();
+
+  Future<String> registerUser(
+      String nama, String profesi, String email, String password) async {
+    final apiUrl = Uri.parse('http://127.0.0.1:80/vigenesia/api/registrasi');
+    final response = await http.post(
+      apiUrl,
+      body: {
+        'nama': nama,
+        'profesi': profesi,
+        'email': email,
+        'password': password
+      },
+    );
+
+    final responseData = json.decode(response.body);
+
+    if (response.statusCode == 200) {
+      bool isActive = responseData['is_active'];
+
+      if (isActive) {
+        return "success";
+      } else {
+        return 'Gagal mendaftar.';
+      }
+    } else if (response.statusCode == 400) {
+      // Jika status kode HTTP adalah 400, kembalikan pesan yang sesuai
+      if (responseData == "The given email already exists.") {
+        return 'Email sudah terdaftar.';
+      } else if (responseData == "Provide complete user info to add.") {
+        return 'Harap lengkapi informasi pengguna.';
+      } else {
+        return 'Terjadi kesalahan.';
+      }
+    } else {
+      return 'Terjadi kesalahan saat menghubungi server.';
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -36,8 +75,8 @@ class _RegisterState extends State<Register> {
                   ),
                   SizedBox(height: 50),
                   FormBuilderTextField(
-                    name: "name",
-                    controller: nameController,
+                    name: "nama",
+                    controller: namaController,
                     decoration: InputDecoration(
                         contentPadding: EdgeInsets.only(left: 10),
                         border: OutlineInputBorder(),
@@ -84,10 +123,26 @@ class _RegisterState extends State<Register> {
                   Container(
                     width: MediaQuery.of(context).size.width,
                     child: ElevatedButton(
-                        onPressed: () {
-                          //pindah kehalaman login
-                          Navigator.push(context,
-                              MaterialPageRoute(builder: (context) => Login()));
+                        onPressed: () async {
+                          String response = await registerUser(
+                              namaController.text,
+                              profesiController.text,
+                              emailController.text,
+                              passwordController.text);
+
+                          if (response == "success") {
+                            //pindah kehalaman login
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => Login()));
+                          } else {
+                            ScaffoldMessenger.of(context).showSnackBar(
+                              SnackBar(
+                                content: Text(response),
+                              ),
+                            );
+                          }
                         },
                         child: Text("Daftar")),
                   ),
