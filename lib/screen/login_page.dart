ca47b638ddb4a4d8f9f5613866c6a45feee5e7a9
Muhammad Hardithya Aenuddin <muhammadadhi2803@gmail.com>
2023-11-01 08:47:53 +0700

    perubahan pada tampilan login dan register

--- lib/screen/login_page.dart
+++ lib/screen/login_page.dart
@@ -1,13 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/gestures.dart';
-import 'register_page.dart';
 import 'package:flutter_form_builder/flutter_form_builder.dart';
 import 'package:form_builder_validators/form_builder_validators.dart';
-import 'dashboard_page.dart';
 import 'dart:convert';
 import 'package:http/http.dart' as http;
+import 'register_page.dart';
+import 'dashboard_page.dart';
 
-// Inisialisasi GlobalKey untuk FormBuilder
 final _formKey = GlobalKey<FormBuilderState>();
 bool _obscureText = true;
 
@@ -22,7 +21,6 @@ class _LoginState extends State<Login> {
   TextEditingController emailController = TextEditingController();
   TextEditingController passwordController = TextEditingController();
 
-  // Fungsi untuk mengirim data login ke API
   Future<String> loginUser(String email, String password) async {
     final apiUrl = Uri.parse('http://127.0.0.1:80/vigenesia/api/login');
     final response = await http.post(
@@ -33,15 +31,8 @@ class _LoginState extends State<Login> {
     final responseData = json.decode(response.body);
 
     if (response.statusCode == 200) {
-      bool isActive = responseData['is_active'];
-
-      if (isActive) {
-        return "success";
-      } else {
-        return 'Gagal.';
-      }
+      return responseData['is_active'] ? "success" : 'Gagal.';
     } else if (response.statusCode == 400) {
-      // Jika status kode HTTP adalah 400, kembalikan pesan yang sesuai
       if (responseData == "Ada kesalahan di email / password.") {
         return 'Login gagal. Email atau password salah.';
       } else if (responseData == "Belum mengisi email dan password.") {
@@ -57,78 +48,96 @@ class _LoginState extends State<Login> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.brown,
-        title: Text('Login'),
-      ),
+      backgroundColor: Color(0xFF4CAF50), // Warna latar belakang hijau
       body: SingleChildScrollView(
-        child: Container(
-          color: Color.fromARGB(255, 255, 255, 255),
-          height: MediaQuery.of(context).size.height,
-          child: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Image.asset('images/4207.jpg', height: 300.0),
-                Text(
-                  'Login',
-                  style: TextStyle(fontSize: 22, fontWeight: FontWeight.w500),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset('images/4207.png',
+                  height: 200.0), // Gambar dari assets
+              Text(
+                'Selamat Datang!',
+                style: TextStyle(
+                  fontSize: 28,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white, // Warna teks putih
                 ),
-                SizedBox(height: 20),
-                Center(
-                  child: FormBuilder(
-                    key: _formKey,
-                    child: Container(
-                      width: MediaQuery.of(context).size.width / 1.3,
-                      child: Column(
+              ),
+              Text(
+                'Silakan masuk untuk melanjutkan',
+                style: TextStyle(
+                  fontSize: 16,
+                  color: Colors.white, // Warna teks putih
+                ),
+              ),
+              SizedBox(height: 20),
+              FormBuilder(
+                key: _formKey,
+                child: Container(
+                  width: MediaQuery.of(context).size.width / 1.3,
+                  child: Column(
+                    children: [
+                      FormBuilderTextField(
+                        name: "email",
+                        controller: emailController,
+                        decoration: InputDecoration(
+                          contentPadding: EdgeInsets.all(16),
+                          border: OutlineInputBorder(),
+                          labelText: "Email",
+                        ),
+                        validator: FormBuilderValidators.compose([
+                          FormBuilderValidators.required(
+                            errorText: 'Email harus diisi',
+                          ),
+                          FormBuilderValidators.email(
+                            errorText: 'Format email tidak valid',
+                          ),
+                        ]),
+                      ),
+                      SizedBox(height: 16),
+                      FormBuilderTextField(
+                        obscureText: _obscureText,
+                        name: "password",
+                        controller: passwordController,
+                        decoration: InputDecoration(
+                          contentPadding: EdgeInsets.all(16),
+                          border: OutlineInputBorder(),
+                          labelText: "Password",
+                          suffixIcon: IconButton(
+                            icon: Icon(
+                              _obscureText
+                                  ? Icons.visibility
+                                  : Icons.visibility_off,
+                            ),
+                            onPressed: () {
+                              setState(() {
+                                _obscureText = !_obscureText;
+                              });
+                            },
+                          ),
+                        ),
+                        validator: FormBuilderValidators.compose([
+                          FormBuilderValidators.required(
+                            errorText: 'Password harus diisi',
+                          ),
+                        ]),
+                      ),
+                      SizedBox(height: 24),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
                         children: [
-                          FormBuilderTextField(
-                            name: "email",
-                            controller: emailController,
-                            decoration: InputDecoration(
-                              contentPadding: EdgeInsets.only(left: 10),
-                              border: OutlineInputBorder(),
-                              labelText: "Email",
-                            ),
-                            validator: FormBuilderValidators.compose([
-                              FormBuilderValidators.required(),
-                              FormBuilderValidators.email(),
-                            ]),
-                          ),
-                          SizedBox(height: 20),
-                          FormBuilderTextField(
-                            obscureText: _obscureText,
-                            name: "password",
-                            controller: passwordController,
-                            decoration: InputDecoration(
-                              contentPadding: EdgeInsets.only(left: 10),
-                              border: OutlineInputBorder(),
-                              labelText: "Password",
-                              suffixIcon: IconButton(
-                                icon: Icon(
-                                  _obscureText
-                                      ? Icons.visibility
-                                      : Icons.visibility_off,
-                                ),
-                                onPressed: () {
-                                  setState(() {
-                                    _obscureText = !_obscureText;
-                                  });
-                                },
-                              ),
-                            ),
-                            validator: FormBuilderValidators.required(),
-                          ),
-                          SizedBox(height: 30),
                           Text.rich(
                             TextSpan(
                               children: [
                                 TextSpan(
-                                  text: 'Tidak ada akun ? ',
-                                  style: TextStyle(color: Colors.black54),
+                                  text: 'Belum punya akun? ',
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                  ),
                                 ),
                                 TextSpan(
-                                  text: 'Sign Up',
+                                  text: 'Daftar sekarang',
                                   recognizer: TapGestureRecognizer()
                                     ..onTap = () {
                                       Navigator.push(
@@ -140,14 +149,20 @@ class _LoginState extends State<Login> {
                                       );
                                     },
                                   style: TextStyle(
-                                    fontWeight: FontWeight.w600,
-                                    color: Colors.blueAccent,
+                                    decoration: TextDecoration.underline,
+                                    color: Color(0xFFFFFFFF),
                                   ),
                                 ),
                               ],
                             ),
                           ),
                           ElevatedButton(
+                            style: ElevatedButton.styleFrom(
+                              primary: Color(0xFF2196F3), // Warna tombol biru
+                              textStyle: TextStyle(
+                                color: Colors.white,
+                              ),
+                            ),
                             onPressed: () async {
                               if (_formKey.currentState?.saveAndValidate() ??
                                   false) {
@@ -166,21 +181,25 @@ class _LoginState extends State<Login> {
                                 } else {
                                   ScaffoldMessenger.of(context).showSnackBar(
                                     SnackBar(
-                                      content: Text(response),
+                                      content: Text(
+                                        response,
+                                        style: TextStyle(color: Colors.white),
+                                      ),
+                                      backgroundColor: Color(0xFFE57373),
                                     ),
                                   );
                                 }
                               }
                             },
-                            child: Text('Login'),
-                          )
+                            child: Text('Masuk'),
+                          ),
                         ],
                       ),
-                    ),
+                    ],
                   ),
                 ),
-              ],
-            ),
+              ),
+            ],
           ),
         ),
       ),
